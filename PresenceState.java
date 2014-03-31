df5264d35d216b73d677985886377df456860951
maradine <maradine@gmail.com>
2014-03-31 15:03:53 -0700

    removed cruft

--- PresenceState.java
+++
@@ -1,9 +0,0 @@
-public class PresenceState {
-	public String channel;
-	public String nickname;
-
-	public PresenceState(String nickname, String channel) {
-		this.nickname = nickname;
-		this.channel = channel;
-	}
-}
